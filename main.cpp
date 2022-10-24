6fd9088ce01752d810ee2d4159e7a4a6a79eadd4
Jan <jpetko1@gmail.com>
2022-10-24 19:11:45 +0200

    Startup/cleanup environment for sockets.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
 int main(int argc, char* argv[])
 {
-  std::cout << "Hello World!\n";
+  WSADATA wsa_data;
+  if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
+  {
+    std::cout << "Startup failed.\n";
+    return 1;
+  }
+
+  if (WSACleanup() != 0)
+  {
+    std::cout << "Cleanup failed.\n";
+    return 1;
+  }
 
   return 0;
 }
