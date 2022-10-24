5c04c8c8bd0c29c5b576d38a5461744dd738bb44
Jan <jpetko1@gmail.com>
2022-10-24 20:34:31 +0200

    Created a socket.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,32 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  auto const ip_str = "TODO";
+  auto const port_num_str = "TODO";
+
+  addrinfo host_info;
+  ZeroMemory(&host_info, sizeof(host_info));
+  host_info.ai_family = AF_UNSPEC;
+  host_info.ai_socktype = SOCK_STREAM;
+  host_info.ai_protocol = IPPROTO_TCP;
+
+  addrinfo* address_info;
+  if (getaddrinfo(ip_str, port_num_str, &host_info, &address_info) != 0)
+  {
+    std::cout << "Get address info failed.\n";
+    WSACleanup();
+    return 1;
+  }
+
+  SOCKET socket_handle = socket(address_info->ai_family, address_info->ai_socktype, address_info->ai_protocol);
+  if (socket_handle == INVALID_SOCKET)
+  {
+    std::cout << "Creating socket failed.\n";
+    freeaddrinfo(address_info);
+    WSACleanup();
+    return 1;
+  }
+
   if (WSACleanup() != 0)
   {
     std::cout << "Cleanup failed.\n";
