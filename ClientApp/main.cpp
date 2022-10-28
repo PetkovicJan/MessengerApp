f067eb730e791f78510feaa7ebd95fc37108cd95
Jan <jpetko1@gmail.com>
2022-10-28 17:27:15 +0200

    Added basic communication between server and client app as shown in Windows documentation.

--- ClientApp/main.cpp
+++ ClientApp/main.cpp
@@ -14,10 +14,21 @@ int main(int argc, char* argv[])
   Address server_address(SocketType::Client, ip_str, port_num_str);
 
   ConnectedSocket socket(server_address);
+  std::cout << "Socket connected to server.\n";
 
   socket.send({ 'H', 'i', '!' });
 
-  auto const received_data = socket.receive();
+  size_t iResult = 0u;
+  do {
+    auto const received_data = socket.receive();
+    iResult = received_data.size();
+    if (iResult > 0)
+      printf("Bytes received: %d\n", iResult);
+    else if (iResult == 0)
+      printf("Connection closed\n");
+    else
+      printf("recv failed: %d\n", WSAGetLastError());
+  } while (iResult > 0);
 
   return 0;
 }
--- ServerApp/main.cpp
+++ ServerApp/main.cpp
@@ -2,6 +2,8 @@
 
 #include "../Socket.h"
 
+#include <thread>
+
 int main(int argc, char* argv[])
 {
   SocketContext context;
@@ -17,11 +19,26 @@ int main(int argc, char* argv[])
   ListeningSocket listening_socket(local_address, 2);
 
   auto const client_socket = listening_socket.accept();
+  std::cout << "Client accepted\n";
 
-  auto const data = client_socket.receive();
+  // Receive until the peer shuts down the connection.
+  size_t iResult = 0u;
+  do {
+    auto const received_data = client_socket.receive();
+    iResult = received_data.size();
+    if (iResult > 0) {
+      printf("Bytes received: %d\n", iResult);
 
-  // Echo data back to the client.
-  client_socket.send(data);
+      // Echo data back to the client.
+      client_socket.send(received_data);
+      printf("Bytes sent: %d\n", iResult);
+    }
+    else if (iResult == 0)
+      printf("Connection closing...\n");
+    else {
+      return 1;
+    }
 
+  } while (iResult > 0);
   return 0;
 }
