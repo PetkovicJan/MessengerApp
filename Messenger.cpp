23897e284ba4a91c4a0917453b91944a11f07d61
Jan <jpetko1@gmail.com>
2022-11-03 20:50:46 +0100

    Removed old MessengerApp class.

--- Messenger.cpp
+++ Messenger.cpp
@@ -2,55 +2,6 @@
 
 #include <iostream>
 
-MessengerApp::MessengerApp(ConnectedSocket& socket) : socket_(socket)
-{
-}
-
-void MessengerApp::start()
-{
-  stop_flag_.store(false);
-
-  auto const receiver_fun = [this]()
-  {
-    while (true)
-    {
-      auto const received_msg = socket_.receive();
-      if (received_msg.size() == 0)
-      {
-        stop_flag_.store(true);
-        return;
-      }
-
-      displayMessage(received_msg);
-    }
-  };
-
-  receiver_ = std::move(std::thread(receiver_fun));
-
-  while (!stop_flag_.load())
-  {
-    auto const msg = queryMessage();
-    socket_.send(msg);
-  }
-
-  if (receiver_.joinable())
-    receiver_.join();
-}
-
-std::string MessengerApp::queryMessage()
-{
-  std::string msg;
-  std::cin >> msg;
-
-  return msg;
-}
-
-void MessengerApp::displayMessage(std::string const& msg)
-{
-  std::lock_guard<std::mutex> lock(output_mtx_);
-  std::cout << msg << '\n';
-}
-
 AppMessage::AppMessage(AppMessageType type, std::optional<std::string> const& opt_data)
 {
   header_.type = type;
--- Messenger.h
+++ Messenger.h
@@ -7,27 +7,6 @@
 #include <mutex>
 #include <atomic>
 
-class MessengerApp
-{
-public:
-  explicit MessengerApp(ConnectedSocket& socket);
-
-  void start();
-
-private:
-  std::string queryMessage();
-
-  void displayMessage(std::string const& msg);
-
-  ConnectedSocket& socket_;
-
-  std::atomic<bool> stop_flag_ = true;
-
-  std::thread receiver_;
-
-  std::mutex output_mtx_;
-};
-
 enum class AppMessageType { UserSentMessage, UserLoggedIn, UserLoggedOut };
 
 class AppMessage
