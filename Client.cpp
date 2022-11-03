1fe1eddbced90c51b519ac12bf5bd82c5ce11450
Jan <jpetko1@gmail.com>
2022-11-03 20:39:05 +0100

    Implemented Client base class.

---
+++ Client.cpp
@@ -0,0 +1,47 @@
+#include "Client.h"
+
+#include <iostream>
+
+Client::Client(std::string const& ip_str, std::string const& port_str)
+{
+  server_ = std::make_unique<ConnectedSocket>(
+    Address(SocketType::Client, ip_str, port_str));
+}
+
+Client::~Client()
+{
+}
+
+void Client::exec()
+{
+  is_alive_.store(true);
+  while (is_alive_.load())
+  {
+    auto const msg = server_->receive();
+    if (msg.size() == 0) break;
+
+    onServerMessageReceived(msg);
+  }
+
+  onDisconnectedFromServer();
+}
+
+void Client::stopClient()
+{
+  is_alive_.store(false);
+}
+
+void Client::sendMessageToServer(std::string const& msg)
+{
+  server_->send(msg);
+}
+
+void Client::onServerMessageReceived(std::string const& msg)
+{
+  std::cout << "Server: " << msg << std::endl;
+}
+
+void Client::onDisconnectedFromServer()
+{
+  std::cout << "Disconnected from server." << std::endl;
+}
--- Client.h
+++ Client.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include "Socket.h"
+
+class Client
+{
+public:
+  explicit Client(
+    std::string const& ip_str, std::string const& port_str);
+  virtual ~Client();
+
+  void exec();
+
+private:
+  // Utility functions, that should be used by the derived classes.
+  void stopClient();
+  void sendMessageToServer(std::string const& msg);
+
+private:
+  // Virtual functions, that should be overridden by derived classes.
+  virtual void onServerMessageReceived(std::string const& msg);
+  virtual void onDisconnectedFromServer();
+
+  std::atomic<bool> is_alive_ = true;
+  std::unique_ptr<ConnectedSocket> server_;
+};
