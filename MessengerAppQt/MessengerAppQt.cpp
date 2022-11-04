84665101ed7ac1d204adc6db1e75bede5ba5d406
Jan <jpetko1@gmail.com>
2022-11-04 20:46:44 +0100

    Added core widgets to the app: users list, input/output text area.

--- MessengerAppQt/MessengerAppQt.cpp
+++ MessengerAppQt/MessengerAppQt.cpp
@@ -1,2 +1,31 @@
 #include "MessengerAppQt.h"
 
+#include <QTextEdit>
+#include <QLayout>
+
+MessengerAppWidget::MessengerAppWidget(QWidget* parent) : QWidget(parent)
+{
+  this->setFixedSize(QSize(800, 600));
+
+  // Create sub-widgets.
+  users_list_view_ = new QListView();
+
+  auto text_input_area = new QTextEdit();
+
+  auto text_output_area = new QTextEdit();
+  text_output_area->setReadOnly(true);
+
+  // Set layout.
+  auto users_layout = new QVBoxLayout();
+  users_layout->addWidget(users_list_view_);
+
+  auto text_layout = new QVBoxLayout();
+  text_layout->addWidget(text_output_area, 2);
+  text_layout->addWidget(text_input_area, 1);
+
+  auto main_layout = new QHBoxLayout();
+  main_layout->addLayout(users_layout, 1);
+  main_layout->addLayout(text_layout, 2);
+
+  this->setLayout(main_layout);
+}
--- MessengerAppQt/MessengerAppQt.h
+++ MessengerAppQt/MessengerAppQt.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <QWidget>
+#include <QListView>
 
 class MessengerAppWidget : public QWidget
 {
   Q_OBJECT
 public:
-  explicit MessengerAppWidget(QWidget* parent = nullptr) {}
+  explicit MessengerAppWidget(QWidget* parent = nullptr);
 
+private:
+  QListView* users_list_view_ = nullptr;
 };
