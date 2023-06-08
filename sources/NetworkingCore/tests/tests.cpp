b29b250c9ce5f20cc27a739415f157febeafcd55
Jan <jpetko1@gmail.com>
2023-06-08 20:27:57 +0200

    Covered MessageBuilder with some more tests.

--- sources/NetworkingCore/tests/tests.cpp
+++ sources/NetworkingCore/tests/tests.cpp
@@ -82,3 +82,59 @@ TEST(MessageBuilderTest, HandlesMultipleCompleteMessagesAllAtOnce)
     ++i;
   }
 }
+
+TEST(MessageBuilderTest, HandlesOneMessageInTwoParts) 
+{
+  // First create a message prefixed with length.
+  const std::string message = "Hello World!";
+  const auto prefixed = detail::prefix_with_length(message);
+
+  // Break message into two parts.
+  const int break_point = prefixed.size() / 2;
+  const auto first_part = prefixed.substr(0, break_point);
+  const auto second_part = prefixed.substr(break_point, prefixed.size() - break_point);
+  ASSERT_EQ(prefixed, first_part + second_part);
+
+  // Create a builder. Build message in two parts.
+  MessageBuilder builder;
+  
+  // Build first part. We expect that result is an empty vector of messages, since no message could be built.
+  const auto result_1 = builder.build(first_part);
+  EXPECT_EQ(result_1.size(), 0);
+  
+  // Build second part. We expect one message that equals the original message.
+  const auto result_2 = builder.build(second_part);
+  ASSERT_EQ(result_2.size(), 1);
+  EXPECT_EQ(result_2.at(0), message);
+}
+
+TEST(MessageBuilderTest, HandlesOneMessageInMoreParts) 
+{
+  // First create a message prefixed with length.
+  const std::string message = "This is a random string: !#$%?";
+  const auto prefixed = detail::prefix_with_length(message);
+
+  const std::vector<int> break_points = { 7, 14, 15, 24, 28 };
+
+  // Create a builder. Build message in more parts.
+  MessageBuilder builder;
+  for (int i = 0; i <= break_points.size(); ++i)
+  {
+    const auto start = i == 0 ? 0 : break_points.at(i - 1);
+    const auto end = i == break_points.size() ? prefixed.size() : break_points.at(i);
+    const auto part = prefixed.substr(start, end - start);
+
+    const auto result = builder.build(part);
+    if (i == break_points.size())
+    {
+      // This is the last part. We expect the message to be reconstructed.
+      ASSERT_EQ(result.size(), 1);
+      EXPECT_EQ(result.at(0), message);
+    }
+    else
+    {
+      // We are not at the end. We expect no message to be reconstructed at this point.
+      EXPECT_EQ(result.size(), 0);
+    }
+  }
+}
