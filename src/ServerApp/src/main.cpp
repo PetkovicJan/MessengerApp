8ccced2e57a4164acbbc3f9f75044bf97f630957
Jan <jpetko1@gmail.com>
2024-11-02 18:08:17 +0100

    Comment server main function.

--- src/ServerApp/src/main.cpp
+++ src/ServerApp/src/main.cpp
@@ -5,18 +5,22 @@
 int main(int argc, char* argv[])
 {
 #ifdef LOOPBACK
-  const char* ip_str = "127.0.0.1";
+  // Loopback address: Packets sent to this address never reach the network, but
+  // are looped through the network interface card only. This is useful for 
+  // testing and development, when running server and client on the same machine.
+  const char* ipStr = "127.0.0.1";
 #else
   // From Windows docs: "if the IP string is an emtpy string, a registered address 
   // on the local computer is returned", which is exactly what we want for the 
-  // server.
-  const char* ip_str = NULL;
+  // server if we want to expose it to the outside world.
+  const char* ipStr = NULL;
 #endif
 
-  // Port number from MS documentation.
-  auto const port_num_str = "27015";
+  // Port number from MS documentation. Note that it could be any of unused ports.
+  char const* portNumStr = "27015";
+  constexpr int MaxNumberOfClients = 10;
+  MessengerServer server(ipStr, portNumStr, MaxNumberOfClients);
 
-  MessengerServer server(ip_str, port_num_str, 5);
   server.exec();
 
   return 0;
